06dbc3fb42de0f0a9780c7a08049ca9627b86038
AlexxDmt <136021261+Alexdmt0110@users.noreply.github.com>
2025-11-03 21:22:02 +0100

    home page

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.html',
-  styleUrl: './home.scss',
+  styleUrls: ['./home.scss']
 })
-export class Home {
+export class HomeComponent {
+  isFading = false;
+  fadeColor: 'duo' | 'groupe' | null = null;
 
+  constructor(private router: Router) {}
+
+  jouer(mode: 'duo' | 'groupe') {
+    this.fadeColor = mode;
+    this.isFading = true;
+
+    setTimeout(() => {
+      this.router.navigate(['/game'], { queryParams: { mode } });
+    }, 1000);
+  }
 }
